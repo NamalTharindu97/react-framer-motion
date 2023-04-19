877a1d5cc6d760abf74e7cc0ebe02c65222f1877
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-19 23:28:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Animation from "./components/animations/Animation";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Animation />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/animations/Animation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./animation.css";
+
+export default function Animation() {
+  return (
+    <div className="outer">
+      <div className="squre"> </div>;
+    </div>
+  );
+}
